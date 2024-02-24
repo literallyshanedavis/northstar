f12c5a3c4e000d297e3de329d4514a064bed129f
shanedavis <literallyshane@gmail.com>
2024-02-24 15:52:02 +0000

    deletedblog

--- .astro/types.d.ts
+++ .astro/types.d.ts
@@ -75,38 +75,8 @@ declare module 'astro:content' {
 	>;
 
 	const entryMap: {
-		"blog": {
-"first-post.md": {
-  id: "first-post.md",
-  slug: "first-post",
-  body: string,
-  collection: "blog",
-  data: InferEntrySchema<"blog">
-} & { render(): Render[".md"] },
-"markdown-style-guide.md": {
-  id: "markdown-style-guide.md",
-  slug: "markdown-style-guide",
-  body: string,
-  collection: "blog",
-  data: InferEntrySchema<"blog">
-} & { render(): Render[".md"] },
-"second-post.md": {
-  id: "second-post.md",
-  slug: "second-post",
-  body: string,
-  collection: "blog",
-  data: InferEntrySchema<"blog">
-} & { render(): Render[".md"] },
-"third-post.md": {
-  id: "third-post.md",
-  slug: "third-post",
-  body: string,
-  collection: "blog",
-  data: InferEntrySchema<"blog">
-} & { render(): Render[".md"] },
-},
-
+		
 	};
 
-	type ContentConfig = typeof import("../src/content/config");
+	type ContentConfig = never;
 }
