4b9586bb67e23199900114bc4cc7fec6a4b8e6a9
shanedavis <literallyshane@gmail.com>
2024-02-24 16:22:31 +0000

    vercel_plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,16 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 import react from "@astrojs/react";
-
+import vercel from '@astrojs/vercel/static';
 // https://astro.build/config
 export default defineConfig({
+  output: 'static',
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+    maxDuration: 8,
+  }),
   integrations: [tailwind({applyBaseStyles: false}), react()],
   experimental: {
     assets: true
